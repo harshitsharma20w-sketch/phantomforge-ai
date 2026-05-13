b394dbc46aa344863745b79266735b2c8ab47855
harshitsharma20w-sketch <harshitsharma20w@gmail.com>
2026-05-13 21:07:48 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const fetch = (...args) =>
+import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const app = express();
 
 app.use(cors());
